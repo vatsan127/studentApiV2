0ca72df311473cf4c5e2cd48ad5f2a2e54a70236
Srivatsan <110617159+vatsan127@users.noreply.github.com>
2024-05-15 20:44:49 +0530

    commit for adding singlestore db connection to project ......

--- src/main/java/com/api/studentApiV2/constant/QueryConstant.java
+++ src/main/java/com/api/studentApiV2/constant/QueryConstant.java
@@ -1,4 +1,9 @@
 package com.api.studentApiV2.constant;
 
 public class QueryConstant {
+    public static final String SELECT_STUDENT = "SELECT * FROM student_info ;";
+    public static final String SELECT_STUDENT_BY_ID = "SELECT * FROM student_info WHERE id = ?;";
+    public static final String INSERT_STUDENT = "INSERT INTO student_info (name, age, degree, branch) VALUES (?,?,?,?) ;";
+    public static final String DELETE_STUDENT = "DELETE FROM student_info WHERE id = ?";
+
 }
--- src/main/java/com/api/studentApiV2/controller/StudentController.java
+++ src/main/java/com/api/studentApiV2/controller/StudentController.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class StudentController {
     private StudentService studentService;
 
+
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
